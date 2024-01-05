ed31e5cfb10ad1841a2e42572a31ba9c88eebc00
Jefferson Lima <jefferson.lima@swift.com.br>
2024-01-05 10:44:08 -0300

    fix: organizando um Context

--- src/components/ExemploContext.tsx
+++ src/components/ExemploContext.tsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import { Header } from "./Header";
-import { CountContext, CountInitialData } from "./contexts/CounterContext";
+import {
+  CountContext,
+  CountInitialData,
+  CountProvider,
+} from "./contexts/CounterContext";
 
 export const ExemploContext = () => {
-  const [onlineCount, setOnlineCount] = useState(92);
+  // const [onlineCount, setOnlineCount] = useState(92);
 
   return (
     <div className="container mx-auto flex max-w-4xl my-8">
-      <CountContext.Provider value={{ onlineCount, setOnlineCount }}>
+      {/* <CountContext.Provider value={{ onlineCount, setOnlineCount }}>
         <Header />
-      </CountContext.Provider>
+      </CountContext.Provider> */}
+
+      <CountProvider>
+        <Header />
+      </CountProvider>
     </div>
   );
 };
